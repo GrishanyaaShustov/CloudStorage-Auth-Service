c7558d444e1883eaa71546a76e140764bd13e319
Grigoriy <dinsuil275283@gmail.com>
2025-12-11 23:50:59 +0500

    Update token config

--- internal/config/tokensConfig.go
+++ internal/config/tokensConfig.go
@@ -6,4 +6,6 @@ type TokensConfiguration struct {
 	JwtSecret       string        `yaml:"jwt_secret" env:"JWT_SECRET"`
 	AccessTokenTTL  time.Duration `yaml:"access_token_ttl" env:"ACCESS_TOKEN_TTL"`
 	RefreshTokenTTL time.Duration `yaml:"refresh_token_ttl" env:"REFRESH_TOKEN_TTL"`
+	Issuer          string        `yaml:"issuer" env:"TOKENS_ISSUER"   env-default:"auth-service"`
+	Audience        string        `yaml:"audience" env:"TOKENS_AUDIENCE" env-default:"cloud-storage-api"`
 }
