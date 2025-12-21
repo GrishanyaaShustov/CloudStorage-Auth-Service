1238e6c5d378f64118216fb0bb798fbe0d1fff2f
Grigoriy <dinsuil275283@gmail.com>
2025-12-21 18:19:49 +0500

    migrate from uuid v4 to v7 for better performance

--- migrations/1_create_users.up.sql
+++ migrations/1_create_users.up.sql
@@ -1,12 +1,8 @@
--- Enable extension for UUID generation
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email TEXT NOT NULL UNIQUE ,
-    password_hash TEXT NOT NULL,
-    registered_at TIMESTAMPTZ NOT NULL DEFAULT now()
+                                     id UUID PRIMARY KEY DEFAULT uuidv7(),
+                                     email TEXT NOT NULL UNIQUE,
+                                     password_hash TEXT NOT NULL,
+                                     registered_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Optional index (ускоряет логин)
 CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
